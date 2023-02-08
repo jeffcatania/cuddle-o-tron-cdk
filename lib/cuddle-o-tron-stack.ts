756b71db7fb17961abea44b404aa38445b30e528
Jeff Catania <jeffreycatania@gmail.com>
2023-02-07 21:44:15 -0500

    added verified email for sender and receiver

--- lib/cuddle-o-tron-stack.ts
+++ lib/cuddle-o-tron-stack.ts
@@ -2,17 +2,23 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { VerifySesEmailAddress } from '@seeebiii/ses-verify-identities';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 
 export class CuddleOTronStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const SES_VERIFIED_SENDER_EMAIL:string = process.env.SES_VERIFIED_SENDER_EMAIL as string;
-
-    const emailAddress = new VerifySesEmailAddress(this, 'SesEmailVerification', {
-      emailAddress: SES_VERIFIED_SENDER_EMAIL
+     new VerifySesEmailAddress(this, 'SesVerifiedSenderEmail', {
+      emailAddress: process.env.SES_VERIFIED_SENDER_EMAIL as string,
+      removalPolicy: RemovalPolicy.RETAIN
     })
+
+    new VerifySesEmailAddress(this, 'SesVerifiedReceiverEmail', {
+      emailAddress: process.env.SES_VERIFIED_RECEIVER_EMAIL as string,
+      removalPolicy: RemovalPolicy.RETAIN
+    })
+
     // example resource
     // const queue = new sqs.Queue(this, 'CuddleOTronQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
