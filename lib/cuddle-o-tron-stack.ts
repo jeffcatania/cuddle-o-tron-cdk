d764227d96128e72e59de078786051ca5acefb8c
Jeff Catania <jeffreycatania@gmail.com>
2023-02-07 21:31:56 -0500

    added VerifySesEmail Address for sender

--- lib/cuddle-o-tron-stack.ts
+++ lib/cuddle-o-tron-stack.ts
@@ -3,12 +3,15 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { VerifySesEmailAddress } from '@seeebiii/ses-verify-identities';
 
+
 export class CuddleOTronStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const SES_VERIFIED_SENDER_EMAIL:string = process.env.SES_VERIFIED_SENDER_EMAIL as string;
+
     const emailAddress = new VerifySesEmailAddress(this, 'SesEmailVerification', {
-      emailAddress: 'hello@example.org'
+      emailAddress: SES_VERIFIED_SENDER_EMAIL
     })
     // example resource
     // const queue = new sqs.Queue(this, 'CuddleOTronQueue', {
