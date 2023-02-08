b4164a1bed70f37dba24773a2645b599ffeeeec9
Jeff Catania <jeffreycatania@gmail.com>
2023-02-07 22:02:44 -0500

    yay - verified identity created

--- test/cuddle-o-tron.test.ts
+++ test/cuddle-o-tron.test.ts
@@ -1,17 +1,26 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CuddleOTron from '../lib/cuddle-o-tron-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CuddleOTron from '../lib/cuddle-o-tron-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cuddle-o-tron-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CuddleOTron.CuddleOTronStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('SES Verified Email Identities', () => {
+   const app = new cdk.App();
+   // WHEN
+   const stack = new CuddleOTron.CuddleOTronStack(app, 'MyTestStack');
+    // THEN
+   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+   test('sender identity created', () => {
+    template.hasResourceProperties('Custom::AWS', {
+        Create: '{"service":"SES","action":"verifyEmailIdentity","parameters":{"EmailAddress":"from@example.com"},"physicalResourceId":{"id":"verify-from@example.com"}}'
+    })
+   })
+
+   test('receiver identity created', () => {
+    template.hasResourceProperties('Custom::AWS', {
+        Create: '{"service":"SES","action":"verifyEmailIdentity","parameters":{"EmailAddress":"to@example.com"},"physicalResourceId":{"id":"verify-to@example.com"}}'
+    })
+   })
+
 });
