b956f5ca6fb78c0c1ebfd3cb062f3cb916584e1c
Jeff Catania <jeffreycatania@gmail.com>
2023-02-07 21:18:30 -0500

    Add code for verify ses email address

--- lib/cuddle-o-tron-stack.ts
+++ lib/cuddle-o-tron-stack.ts
@@ -1,13 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { VerifySesEmailAddress } from '@seeebiii/ses-verify-identities';
 
 export class CuddleOTronStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
+    const emailAddress = new VerifySesEmailAddress(this, 'SesEmailVerification', {
+      emailAddress: 'hello@example.org'
+    })
     // example resource
     // const queue = new sqs.Queue(this, 'CuddleOTronQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
