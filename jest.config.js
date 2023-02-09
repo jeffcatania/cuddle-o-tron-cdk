81cc2a271631e16e34b843881e5fc2077543ef03
Jeff Catania <jeffreycatania@gmail.com>
2023-02-08 21:24:44 -0500

    yay prettified all the things on pre-commit hook

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    "^.+\\.tsx?$": "ts-jest",
   },
-  setupFiles: ["<rootDir>/.jest/setupEnv.js"]
+  setupFiles: ["<rootDir>/.jest/setupEnv.js"],
 };
--- lib/cuddle-o-tron-stack.ts
+++ lib/cuddle-o-tron-stack.ts
@@ -1,26 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { VerifySesEmailAddress } from '@seeebiii/ses-verify-identities';
-import { RemovalPolicy } from 'aws-cdk-lib';
-import { EmailReminderLambdaConstruct } from './email-reminder-lambda-construct';
-
+import { VerifySesEmailAddress } from "@seeebiii/ses-verify-identities";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { EmailReminderLambdaConstruct } from "./email-reminder-lambda-construct";
 
 export class CuddleOTronStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     new VerifySesEmailAddress(this, 'SesVerifiedSenderEmail', {
+    new VerifySesEmailAddress(this, "SesVerifiedSenderEmail", {
       emailAddress: process.env.SES_VERIFIED_SENDER_EMAIL as string,
-      removalPolicy: RemovalPolicy.RETAIN
-    })
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
 
-    new VerifySesEmailAddress(this, 'SesVerifiedReceiverEmail', {
+    new VerifySesEmailAddress(this, "SesVerifiedReceiverEmail", {
       emailAddress: process.env.SES_VERIFIED_RECEIVER_EMAIL as string,
-      removalPolicy: RemovalPolicy.RETAIN
-    })
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
 
-    new EmailReminderLambdaConstruct(this, 'EmailReminderService', {})
+    new EmailReminderLambdaConstruct(this, "EmailReminderService", {});
 
     // example resource
     // const queue = new sqs.Queue(this, 'CuddleOTronQueue', {
